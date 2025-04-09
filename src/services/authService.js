42cfa79a77ce07a436a9e429671ba78148fd19bc
hatrongvu13 <hatrongvu13@gmail.com>
2025-04-09 15:42:36 +0700

    update file build

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-import {AUTH_TOKEN, REFRESH_TOKEN} from "../constant/Constant.js";
+import {AUTH_TOKEN} from "../constant/Constant.js";
 
-const API_URL = 'http://localhost:8080/user-manager/api/v1/user';
+const API_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
     baseURL: API_URL,
